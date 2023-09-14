850d6a5853c19afa27fe31cbe694ad8fcd8423a6
ASUS <zubbaerhossain@gmail.com>
2023-09-14 11:45:11 +0600

    1st commit. here I add Tailwind & init 4 component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { PostDetailsComponent } from './post-details/post-details.component';
+import { AddCommentComponent } from './add-comment/add-comment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    PostDetailsComponent,
+    AddCommentComponent
   ],
   imports: [
     BrowserModule,
